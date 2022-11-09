aed26c4dc47d0f7adaec44f912503ecabaed6db0
minjoo999 <68040390+minjoo999@users.noreply.github.com>
2022-11-09 17:54:30 +0900

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+      <About />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/About.js
@@ -0,0 +1,13 @@
+import Detail from "../components/Detail";
+import MorePictures from "../components/MorePictures";
+import React from "react";
+
+export function About() {
+  return (
+    <div>
+      <h1> About test</h1>
+      <Detail />
+      <MorePictures />
+    </div>
+  );
+}
