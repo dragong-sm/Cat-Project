6f8be979b30aabb62f263ef8408369954e64f1b7
dragong-sm <117700630+dragong-sm@users.noreply.github.com>
2022-11-10 11:05:40 +0900

    ver01
    깃허브 너무 어렵다 ..

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,16 @@ import logo from "./logo.svg";
 import "./App.css";
 import { Home } from "./pages/Home";
 import { About } from "./pages/About";
+import { useState } from "react";
 
 function App() {
   return (
     <div className="App">
       <Home />
       <About />
-      <h1>안녕하세요</h1>
+      {/* 1. 주요 5 고양이 : Home에서 사진 클릭하면 id? catname? About에서 받기 */}
+      {/* 2. Other : Home에서 이름을 클릭하면 id? catname? About에서 받기 */}
+      {/* 3. aboutToggle이 true이면 해당 고양이 about 페이지로 이동 ! */}
     </div>
   );
 }
