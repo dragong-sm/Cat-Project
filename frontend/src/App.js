cb042ddb715725b5ae8e94794256df62149b1677
dragong-sm <117700630+dragong-sm@users.noreply.github.com>
2022-11-10 16:00:46 +0900

    11월10일 오후4시
    ㅇ

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,37 @@
 import logo from "./logo.svg";
+import React, { useEffect } from "react";
 import "./App.css";
 import { Home } from "./pages/Home";
 import { About } from "./pages/About";
 import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import { getData, axiosData } from "./api/getCats";
+import data from "./db/mock.json";
 
 function App() {
+  // 데이터는 db로 불러오기 !
+  const [cats, setCats] = useState([]);
+  useEffect(() => {
+    // const data = getData();
+    // setCats(data);
+
+    // axios 사용
+    const data = axiosData();
+    data.then((data) => setCats(data));
+  }, []);
+
   return (
     <div className="App">
-      <Home />
-      <About />
+      {/* <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about/*" element={<About />} />
+      </Routes> */}
       {/* 1. 주요 5 고양이 : Home에서 사진 클릭하면 id? catname? About에서 받기 */}
       {/* 2. Other : Home에서 이름을 클릭하면 id? catname? About에서 받기 */}
       {/* 3. aboutToggle이 true이면 해당 고양이 about 페이지로 이동 ! */}
+
+      <h1>About-test</h1>
+      <About cats={cats} />
     </div>
   );
 }
---
+++ frontend/src/styles/Style.js
@@ -0,0 +1,72 @@
+import React from "react";
+import styled from "styled-components";
+
+export const Wrapper = styled.section`
+  height: 950px;
+  width: 100%;
+  padding: 0.5px;
+  background: #dff6ff;
+`;
+
+export const Article = styled.h1`
+  height: 5%;
+  padding: 10px;
+  margin: 10px;
+  font-size: 1.5em;
+  border: 5px solid gray;
+  border-style: none none double none;
+  //   background: yellow;
+`;
+
+export const PreviousPage = styled.section`
+  height: 3%;
+  margin: 10px;
+  padding: 10px;
+  text-align: right;
+  //   background: violet;
+`;
+
+export const Main = styled.section`
+  height: 55%;
+  padding: 10px;
+  margin: 10px;
+  //   background: blue;
+`;
+
+export const More = styled.section`
+  height: 20%;
+  padding: 10px;
+  margin: 10px;
+  background: red;
+`;
+
+export const MainImage = styled.img`
+  height: 90%;
+  width: 40%;
+  margin-top: 1%;
+  margin-left: 10%;
+  float: left;
+  border: 10px solid skyblue;
+  border-style: inset;
+  border-radius: 30px;
+`;
+
+export const Contents = styled.div`
+  height: 90%;
+  width: 30%;
+  padding: 10px;
+  margin-top: 1%;
+  margin-right: 10%;
+  float: right;
+  font-size: 15px;
+  text-align: left;
+  //   background: green;
+`;
+
+export const MoreImage = styled.img`
+  width: 25%;
+  height: 80%;
+  margin: 10px;
+  padding: 10px;
+  background: blue;
+`;
